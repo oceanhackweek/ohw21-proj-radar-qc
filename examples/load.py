147d0afc9bf829cb0809687a574daa2d9fbf0bf8
John Stanco <stancoj9@gmail.com>
2021-08-06 10:38:57 -0400

    Add example script for comparing filtering methods

--- examples/load.py
+++ examples/load.py
@@ -1,23 +1,88 @@
-from radarqc.csfile import CSFile
-
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import time
+
+from radarqc.csfile import CSFile
+from radarqc.dataset import DataSet
+from radarqc.filtering import (
+    NoiseFilter,
+    PCAFilter,
+    PreFitPCAFilter,
+    SpectrumFilter,
+)
+
+from radarqc.processing import (
+    Abs,
+    CompositeProcessor,
+    GainCalculator,
+    Normalize,
+    SignalProcessor,
+)
+
+from radarqc import csfile
+from typing import Iterable
+
+
+def plot_vertical(*images) -> None:
+    image = np.concatenate(images)
+    plt.imshow(image, aspect=4)
+    plt.colorbar()
+    plt.show()
+
+
+def _time(fn, *args, **kwargs) -> tuple:
+    t = time.time()
+    result = fn(*args, **kwargs)
+    return result, time.time() - t
+
+
+def log_timing(t: float, name: str) -> None:
+    line_length = 80
+    print("-" * line_length)
+    print("Time taken for {}: {} sec".format(name, t))
+    print("-" * line_length)
+
+
+def create_preprocessor() -> SignalProcessor:
+    reference_gain = 34.2
+    return CompositeProcessor(
+        Abs(), GainCalculator(reference=reference_gain), Normalize()
+    )
+
+
+def compare_filters(
+    spectrum: np.ndarray, filters: Iterable[SpectrumFilter]
+) -> None:
+    for filt in filters:
+        name = filt.__class__.__name__
+        filtered, t = _time(filt, spectrum)
+        log_timing(t, name)
+        plot_vertical(spectrum, filtered)
+
+
+def generate_paths(dir: str) -> Iterable[str]:
+    return glob.glob(os.path.join(dir, "*.cs"))
 
 
 def main():
-    path = "../../codar/CSS_ASSA_21_05_23_0030.cs"
+    base = "../../codar"
+    paths = generate_paths(base)
+    preprocess = create_preprocessor()
+
+    dataset = DataSet(paths, preprocess)
+    num_components = 0.8
+
+    noise = NoiseFilter(threshold=0.188, window_std=0.005)
+    pca = PCAFilter(num_components=num_components)
+    prefit = PreFitPCAFilter(dataset.spectra, num_components)
+
+    path = "../../codar/CSS_ASSA_21_06_26_1400.cs"
     with open(path, "rb") as f:
-        cs = CSFile.load_from(f)
-        print(cs.header)
-
-        spectrum = cs.spectrum.to_numpy()
-        spectrum = (spectrum - spectrum.min()) / (
-            spectrum.max() - spectrum.min()
-        )
-
-        spectrum = np.log(1 + spectrum) / np.log(2)
-        plt.imshow(spectrum)
-        plt.show()
+        cs = csfile.load(f, preprocess)
+        filters = [prefit, pca, noise]
+        compare_filters(cs.antenna3, filters)
 
 
 if __name__ == "__main__":
