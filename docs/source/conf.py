d3baa832e23198a72ada03b4c6654f65aca4111a
John Stanco <stancoj9@gmail.com>
2021-08-06 16:11:27 -0400

    Update sphinx path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../radarqc"))
+path = os.path.join("..", "..")
+sys.path.insert(0, os.path.abspath(path))
 
 
 # -- Project information -----------------------------------------------------
