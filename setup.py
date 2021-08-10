9b3526940d02e8a9418d580ff7d6f09c5084c9c3
John Stanco <stancoj9@gmail.com>
2021-08-10 04:21:40 -0400

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name="radarqc",
     version="0.1.0",
     author="John Stanco",
+    description="Python package for loading and processing HF radar spectra in Cross-Spectrum file format",
     long_description=long_description,
     packages=find_packages(),
 )
